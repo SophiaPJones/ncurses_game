eaf536a52a3edbf50684f56887148d39eaa53fb0
Sean Jone <seanpjones23@gmail.com>
2019-08-05 19:09:16 -0400

    inputstuff

--- game.cpp
+++ game.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
 #include <ncurses.h>
+#include <unordered_set>
+#include <thread>
+#include <chrono>
+
 
 int main()
 {
+  std::unordered_set<char> curinput;
   initscr();
-  printw("Hello world!");
+  nodelay(stdscr, TRUE);
+  raw();
+  noecho();
+  keypad(stdscr, TRUE);
+  while(true) //gamerunning
+    {
+      char c;
+      while((c = getch()) != ERR)
+        {
+          curinput.insert(ERR);
+        }
+
+      printw("currently pressed buttons: ");
+      for(auto elem : curinput)
+        {
+          printw("%c",elem);
+        }
+      if(curinput.find((char)27) != curinput.end())
+        {
+          break;
+        }
+      refresh();
+      curinput.clear();
+      std::this_thread::sleep_for(1);
+    }
   refresh();
   getch();
   endwin();
